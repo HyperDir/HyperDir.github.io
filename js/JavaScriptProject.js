baeec16e3b70c284f699973f204a7da540478410
GrootDir <elie@lahoodfamily.com.au>
2024-05-30 12:00:22 +1000

    Rewrote JS Game

--- js/JavaScriptProject.js
+++ js/JavaScriptProject.js
@@ -1,96 +1,86 @@
 const canvas = createCanvas(1920 , 1088);
 const gridSize = 64;
 
-var currentDir = [1, 0];
-var currentSize = 4;
-var coveredTiles = [vectorToIndex(5, 8)];
-var applePosition = vectorToIndex(21, 8);
+let currentDir = [1, 0];
+const coveredTiles = [[5, 8], [4, 8], [3, 8]];
+let applePosition = [21, 8];
 
 function createCanvas(width, height) {
-    var c = document.createElement('canvas');
+    let c = document.createElement('canvas');
     c.setAttribute('width', width);
     c.setAttribute('height', height);
     return c;
 }
 
-function resize(arr, newSize, defaultValue) {
-    while(newSize > arr.length)
-        arr.push(defaultValue);
-    arr.length = newSize;
-}
-
 document.body.appendChild(canvas);
+
 document.addEventListener('keydown', function(event) {
-    if(event.key == "ArrowUp") {
-        currentDir = [0, -1]
-    }
-    else if(event.key == "ArrowRight") {
-        currentDir = [1, 0]
-    }
-    else if(event.key == "ArrowDown") {
-        currentDir = [0, 1]
-    }
-    else if(event.key == "ArrowLeft") {
-        currentDir = [-1, 0]
-    }
+  let newDir = [0, 0];
+  if(event.key === "ArrowUp") {
+    newDir = [0, -1]
+  } else if(event.key === "ArrowRight") {
+    newDir = [1, 0]
+  } else if(event.key === "ArrowDown") {
+    newDir = [0, 1]
+  } else if(event.key === "ArrowLeft") {
+    newDir = [-1, 0]
+  }
+
+  if (coveredTiles[1][0] !== coveredTiles[0][0] + newDir[0] || coveredTiles[1][1] !== coveredTiles[0][1] + newDir[1]) {
+    currentDir = newDir;
+  }
 });
 
+function isValid(pos) {
+  return isValidCoords(pos[0], pos[1]);
+}
+function isValidCoords(x, y) {
+  for (let pos of coveredTiles) {
+    if (pos[0] === x && pos[1] === y) {
+      return false;
+    }
+  }
+  return x >= 0 && y >= 0 && x < canvas.width / gridSize && y < canvas.height / gridSize;
+}
+
+function draw(canvas, position, colour, border) {
+  canvas.fillStyle = border;
+  canvas.fillRect(position[0] * gridSize, position[1] * gridSize, gridSize, gridSize);
+  canvas.fillStyle = colour;
+  canvas.fillRect(position[0] * gridSize + 8, position[1] * gridSize + 8, gridSize - 16, gridSize - 16);
+}
 setInterval(
   function() {
-    var currentPos = coveredTiles[0];
-    var newPos = currentPos + vectorToIndex(currentDir[0], currentDir[1]);
+    let newPos = [coveredTiles[0][0] + currentDir[0], coveredTiles[0][1] + currentDir[1]];
+    if (!isValid(newPos)) {
+      alert(`Failed!\nScore: ${coveredTiles.length - 3}`);
+      coveredTiles.length = 3;
+      coveredTiles[0] = [5, 8];
+      coveredTiles[1] = [4, 8];
+      coveredTiles[2] = [3, 8];
 
-    if(!coveredTiles.includes(newPos) && !(Math.abs(indexToVector(currentPos)[0] - indexToVector(newPos)[0] + indexToVector(currentPos)[1] - indexToVector(newPos)[1]) > 1)) {
-      coveredTiles.unshift(newPos);
-      coveredTiles.length = currentSize;
-    } else {
-      alert("You Died!")
-      coveredTiles = [vectorToIndex(5, 8)];
-      currentDir = [1, 0]
-      currentSize = 4;
-      applePosition = vectorToIndex(21, 8)
+      currentDir = [1, 0];
+      applePosition = [21, 8];
+      return;
     }
+    coveredTiles.unshift(newPos);
 
-    if(coveredTiles.includes(applePosition)) {
-      currentSize++
-      while(coveredTiles.includes(applePosition)) {
-        applePosition = Math.floor(canvas.width * canvas.height / gridSize / gridSize * Math.random())
+    if (!isValid(applePosition)) {
+      while(!isValid(applePosition)) {
+        applePosition = [Math.floor(canvas.width / gridSize * Math.random()), Math.floor(canvas.height / gridSize * Math.random())]
       }
+    } else {
+      coveredTiles.pop();
     }
+    let ctx = canvas.getContext("2d");
 
-    var ctx = canvas.getContext("2d");
     ctx.fillStyle = "#666666";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    for (let x = 0; x < Math.floor(canvas.width / gridSize); x++) {
-      for (let y = 0; y < Math.floor(canvas.height / gridSize); y++) {
-        ctx.fillStyle = "#00000000"
-        if(coveredTiles[0] == vectorToIndex(x, y)) {
-          ctx.fillStyle = "#ffffff";
-        } else if(coveredTiles.includes(vectorToIndex(x, y))) {
-          ctx.fillStyle = "#88ff88";
-        } else if(vectorToIndex(x, y) == applePosition) {
-          ctx.fillStyle = "#ff8888";
-        }
-        ctx.fillRect(
-          x * gridSize,
-          y * gridSize,
-          gridSize,
-          gridSize
-        );
-      }
+
+    draw(ctx, applePosition, '#AA4A44', '#8B0000');
+    for (let v of coveredTiles) {
+      draw(ctx, v, '#44AA4A', '#008B00');
     }
   },
   100
-);
-
-function vectorToIndex(x, y) {
-  return (x + y * Math.floor(canvas.width / gridSize))
-}
-
-function indexToVector(index) {
-  index %= (canvas.width * canvas.height / gridSize / gridSize);
-  return [
-    Math.floor(index % (canvas.width / gridSize)),
-    Math.floor(index / (canvas.width / gridSize))
-  ];
-}
+)
